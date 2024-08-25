3b9ff605dc2d67e2ef19fd00ff4614287fb7fec4
Daniil Zolotarev <maikushaloves@yandex.ru>
2024-08-25 19:15:33 +0300

    addCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content; 
 // @todo: DOM узлы
-
+const listCard = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(element, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const buttonDelete = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').scr = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    buttonDelete.addEventListener('click', deleteCard);
+    return cardElement
+}
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+    const target = evt.target.closest('.card');
+    target.remove()
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(element) {
+    listCard.append(createCard(element, deleteCard))
+});
+
